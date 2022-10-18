a0ff9fc98dfe98fa857ed8af8a058da3baa4d0a1
Админ <KorostelevDima@yandex.ru>
2022-10-18 22:02:29 +0200

    test commit

--- main.py
+++ main.py
@@ -1,84 +1,21 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore_async
 
-import os
+# Use the application default credentials.
+cred = credentials.ApplicationDefault()
 
-import flask
+firebase_admin.initialize_app(cred)
+db = firestore_async.client()
 
 
-def hello_world(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
-    return "Hello World!"
+async def add_document(doc_ref, new_doc):
+    await doc_ref.set(new_doc)
 
+doc_ref = db.collection("users").document("alovelace")
+new_doc = {"first": "Ada", "last": "Lovelace", "born": 1815}
+add_document(doc_ref, new_doc)
 
-def hello_name(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
-    """
-    request_args = request.args
-
-    if request_args and "name" in request_args:
-        name = request_args["name"]
-    else:
-        name = "World"
-    return "Hello {}!".format(flask.escape(name))
-
-
-def python_powered(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
-    Returns:
-        The response file, a JPG image that says "Python Powered"
-    """
-    return flask.send_from_directory(
-        os.getcwd(), "python_powered.jpg", mimetype="image/jpg"
-    )
-
-# import firebase_admin
-# from firebase_admin import credentials
-# from firebase_admin import firestore_async
-#
-# # Use the application default credentials.
-# cred = credentials.ApplicationDefault()
-#
-# firebase_admin.initialize_app(cred)
-# db = firestore_async.client()
-#
-#
-# async def add_document(doc_ref, new_doc):
-#     await doc_ref.set(new_doc)
-#
-# doc_ref = db.collection("users").document("alovelace")
-# new_doc = {"first": "Ada", "last": "Lovelace", "born": 1815}
-# add_document(doc_ref, new_doc)
-#
-# doc_ref = db.collection("users").document("aturing")
-# new_doc = {"first": "Alan", "middle": "Mathison", "last": "Turing", "born": 1912}
-# add_document(doc_ref, new_doc)
+doc_ref = db.collection("users").document("aturing")
+new_doc = {"first": "Alan", "middle": "Mathison", "last": "Turing", "born": 1912}
+add_document(doc_ref, new_doc)
