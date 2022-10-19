c104d9c3b2b0cafa0efb94d2a8d1cc0abef1eadf
Dmitr213 <KorostelevDima@yandex.ru>
2022-10-19 16:48:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-import flask
+from flask import escape
 import functions_framework
 
 @functions_framework.http
-def hello_world(request):
+def hello_http2(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
         The response text, or any set of values that can be turned into a
         Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    return "Hello World!"
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
 
 
 # import firebase_admin
